3852fbd59eb3349f854438afbbbc0160a633b1ab
Alex Lourie <alex.lourie@zepben.com>
2024-07-17 13:32:51 +0930

    Fixups after PR; update /etc/timezone
    Signed-off-by: Alex Lourie <alex.lourie@zepben.com>

--- pipelines/init.sh
+++ pipelines/init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-mkdir -p scripts
+mkdir -p /scripts
 rm -rf /etc/localtime
 ln -s /usr/share/zoneinfo/Australia/Sydney /etc/localtime
+
+# This supposed to be unnecessary, but there might be old things java that need it
+# TODO: probably remove when moved over to newer Java (>11)
+echo "Australia/Sydney" > /etc/timezone

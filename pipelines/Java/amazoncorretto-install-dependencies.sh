4777b78ea2a36559c0afc59f45487fb45b962a76
Alex Lourie <alex.lourie@zepben.com>
2025-02-06 15:41:33 +1030

    Install libxml2-utils in the java pipeline container
    Signed-off-by: Alex Lourie <alex.lourie@zepben.com>

--- pipelines/Java/amazoncorretto-install-dependencies.sh
+++ pipelines/Java/amazoncorretto-install-dependencies.sh
@@ -3,6 +3,7 @@ wget -O - https://apt.corretto.aws/corretto.key | gpg --dearmor -o /usr/share/ke
     echo "deb [signed-by=/usr/share/keyrings/corretto-keyring.gpg] https://apt.corretto.aws stable main" | tee /etc/apt/sources.list.d/corretto.list
 apt -y update && apt install --no-install-recommends -y \
  java-11-amazon-corretto-jdk \
+ libxml2-utils \
  xmlstarlet \
  jq \
  gnupg \

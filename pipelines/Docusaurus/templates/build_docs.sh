eabfef67973931c594126a7bbcd9c4973a2db54e
Alex Lourie <alex.lourie@zepben.com>
2025-07-08 17:31:13 +0930

    Reorg
    Signed-off-by: Alex Lourie <alex.lourie@zepben.com>

--- pipelines/Docusaurus/templates/build_docs.sh
+++ pipelines/Docusaurus/templates/build_docs.sh
@@ -1,38 +1,38 @@
 #!/usr/bin/env bash
 
-# We assume that the docs folder is mounted under /docs
-cd /docs
+# We assume that the docs folder is mounted under current directory.
+# Container will try to use /docs (easy for local builds)
+# CI will use whatever . is
 
 # site-config.json provides the values that we put into the package.json and docusaurus.config.js
 # These values in most cases are just the name of the project, slug (where to access it in the doco site) 
 # and the "project" meta tag. 
 
 # Check that site-config.json is provided
-if [ ! -f site-config.json ]; then
-    echo "The file site-config.json seems to be missing! Please check and try again!" 
-    exit 1
-fi
-
-# TODO: move release-notes into the root path
-# the reason is that we only need them for build time, not actually in src/pages
-# Backup the release-notes
 release_notes="./src/pages/release-notes.md"
-if [ -f "${release_notes}" ]; then
-    cp "${release_notes}" .
-    rm -rf src
-    rm -rf build
-    rm -rf node_modules
+if [ -f site-config.json ]; then
+    # We're building Docusaurus 3
+
+    if [ ! -f release-notes.md ]; then
+        echo "The file release-notes.json seems to be missing! Please check and try again!" 
+        exit 1
+    fi
+
+    # Move the templates and place release-notes in src/pages
+    cp -r /templates/* .
+    cp release-notes.md "${release_notes}"
+
+    # parse templates
+    eval "$(jq -r 'to_entries[] | "export \(.key)=\"\(.value)\""' site-config.json)"
+    sed -e "s/{title}/${title}/g" -e "s/{slug}/${slug}/g" -e "s/{projectName}/${projectName}/g" docusaurus.config.js.template > ./docusaurus.config.js
+    sed -e "s/{projectName}/${projectName}/g" package.json.template > ./package.json
+
+    # cleanup
+    rm -rf *template*
+    rm -rf build-docs.sh
 fi
 
-# Move the templates and restore release-notes
-cp -r /templates/* .
-cp release-notes.md "${release_notes}"
-
-eval "$(jq -r 'to_entries[] | "export \(.key)=\"\(.value)\""' site-config.json)"
-sed -e "s/{title}/${title}/g" -e "s/{slug}/${slug}/g" -e "s/{project}/${project}/g" docusaurus.config.js.template > ./docusaurus.config.js
-sed -e "s/{name}/${name}/g" package.json.template > ./package.json
-
-rm -rf *template*
-
-npm ci
-npm run build
+if [ "$1" != "skip-build" ]; then 
+    npm ci
+    npm run build
+fi

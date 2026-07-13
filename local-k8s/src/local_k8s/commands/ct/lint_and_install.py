56af5e595d3f50b26dc34756fe3e7dbea9b839c9
William Coe <william.coe@zepben.com>
2026-07-13 00:01:27 +0000

    Create namespace before lint/test
    This is required as we must install our image-pull-secret into the
    namespace before we pull any custom images. ct (chart testing)
    randomises the namespaces by default, which prevents the tests for
    working. This feature is primarily useful for supporting concurrent
    tests in different namespaces, which is not a feature we need as we have
    a single chart per repo (mostly) and not very many tests. By specifying
    the test namespace in the ct.yaml and creating it in this tool before
    executing any tests, we can ensure that we create our image creds in the
    correct location.
    
    Signed-off-by: William Coe <william.coe@zepben.com>

--- local-k8s/src/local_k8s/commands/ct/lint_and_install.py
+++ local-k8s/src/local_k8s/commands/ct/lint_and_install.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from subprocess import CalledProcessError
 
 import click
+import yaml
 from click import ClickException
 
 from local_k8s.shared import execute
@@ -25,18 +26,36 @@ def lint_and_install(helm_dir: Path) -> None:
     with chdir(helm_dir.absolute()):
         ct_yaml_path = Path(CT_YAML)
         if not ct_yaml_path.exists():
-            raise ClickException("ct.yaml is required in the root of --helm-dir")
-        try:
-            execute(
-                "ct",
-                "lint-and-install",
-                "--config",
-                "ct.yaml",
-                "--all",
-                "--check-version-increment=false",
-                capture_stdout=False,
-            )
-        except CalledProcessError as e:
-            raise ClickException(
-                f"lint-and-install failed with rc={e.returncode}"
-            ) from e
+            raise ClickException(f"{CT_YAML} is required in the root of --helm-dir")
+
+        create_test_namespace(ct_yaml_path)
+        execute_lint_and_install()
+
+
+def create_test_namespace(ct_yaml_path: Path) -> None:
+    ct_yaml = yaml.safe_load(ct_yaml_path.read_text())
+    test_namespace = ct_yaml.get("namespace")
+    if test_namespace is None:
+        raise ClickException(f"namespace must be specified in {CT_YAML}")
+    namespaces = execute("kubectl", "get", "namespaces")
+    for line in namespaces.splitlines():
+        ns, *_ = line.split()
+        if ns == test_namespace:
+            break
+    else:
+        execute("kubectl", "create", "namespace", test_namespace)
+
+
+def execute_lint_and_install() -> None:
+    try:
+        execute(
+            "ct",
+            "lint-and-install",
+            "--config",
+            "ct.yaml",
+            "--all",
+            "--check-version-increment=false",
+            capture_stdout=False,
+        )
+    except CalledProcessError as e:
+        raise ClickException(f"lint-and-install failed with rc={e.returncode}") from e
